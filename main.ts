d167b415ca42cdcfc59a4854b160bedcf51177c1
lhlyu <misakafs@gmail.com>
2023-04-20 16:52:46 +0800

    init

--- main.ts
+++ main.ts
@@ -78,7 +78,7 @@ const headers = new Headers({
     "Content-Type": "application/json",
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
-    "Access-Control-Allow-Headers": "Content-Type",
+    "Access-Control-Allow-Headers": "Content-Type, Authorization, AUTH_CODE",
 });
 
 serve(async (request: Request) => {
